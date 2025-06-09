bd542c00c456f45bf50d92573c3af699b483e753
iviy10 <nixiex77@gmail.com>
2025-06-09 16:07:14 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function toggleTheme() {
+  document.body.classList.toggle('roxo');
+}
+
+function openLogin() {
+  document.getElementById('loginModal').classList.remove('hidden');
+}
+
+function closeLogin() {
+  document.getElementById('loginModal').classList.add('hidden');
+}
